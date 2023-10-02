d2f2720c25227d7fce8430af18c56e0dc7aed18d
AbrahamEhonwa1 <138295382+AbrahamEhonwa1@users.noreply.github.com>
2023-10-02 15:16:47 +0100

    Add files via upload

---
+++ sixth.java
@@ -0,0 +1,19 @@
+package coding101;
+
+public class sixth {
+	public static void main(String [] args)
+	{
+	int count=10;
+	
+	do
+	{
+		System.out.println(count+" in a bed and the little one said,");
+		System.out.println("Roll over, rollover");
+		System.out.println("They all rolled over and one fell out,");
+		System.out.println();
+		count=count-1;
+	}while(count>=1);
+	System.out.println("Alone at last");
+	}
+	//do while loop 
+}
