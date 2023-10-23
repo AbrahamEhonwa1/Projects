611ea7c5ec5eb3bf7c3d31af7574040af3b65314
AbrahamEhonwa1 <138295382+AbrahamEhonwa1@users.noreply.github.com>
2023-10-23 17:49:06 +0100

    Add files via upload

---
+++ Input System.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ string x;
+ int y;
+ 
+ cout<<"what is your name"<<endl;
+ cin>>x;
+ 
+ cout<<"What is your age"<<endl;
+ 
+ cin>>y;
+ 
+ cout<<x<<" is "<<y<<" years old";
+ return 0;
+}
