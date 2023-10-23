3ba12b27b635e7490e1257032912f1aa6a74f876
AbrahamEhonwa1 <138295382+AbrahamEhonwa1@users.noreply.github.com>
2023-10-23 16:05:41 +0100

    Add files via upload

---
+++ Multiplication.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+    float myvar= 10.5;
+ int x=5;cout << x<<endl;
+ 
+ int y=10; cout<< y<< endl;
+ 
+ int z=y*2, w=x+z;//just adding and multiplying number togther
+ 
+ cout <<z<< endl<<w<<endl;
+}
